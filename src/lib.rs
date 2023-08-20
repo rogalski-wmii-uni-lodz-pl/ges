c83fce464f8a42421f4729c8647b435a0bd66b7b
Marek <marek@combopt.org>
2023-08-20 08:10:09 +0200

    Add config

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,35 @@ use data::Data;
 use evaluator::Evaluator;
 use sol::Sol;
 use stats::Stats;
+use std::time::Duration;
 
 pub mod data;
 pub mod eval;
 pub mod evaluator;
 pub mod mov;
+pub mod routes;
 pub mod sol;
 pub mod stats;
-pub mod routes;
 
 const UNSERVED: usize = usize::MAX;
 pub const K_MAX: usize = 10;
-pub const TOTAL_TIME: u64 = 600;
+// pub const TOTAL_TIME: u64 = 600;
 
 pub struct Ges<'a> {
     evaluator: Evaluator<'a>,
     stats: Stats,
+}
+
+pub enum Log {
+    Quiet,
+    Verbose,
+    Extra,
+}
+
+pub struct Conf {
+    pub max_optimization_time: Duration,
+    pub target_routes: usize,
+    pub log: Log,
 }
 
 impl<'a> Ges<'a> {
@@ -28,17 +41,22 @@ impl<'a> Ges<'a> {
         }
     }
 
-    pub fn ges(&mut self, solution: &mut Sol, target_routes: usize) {
+    pub fn ges(&mut self, solution: &mut Sol, conf: Conf) {
         loop {
             let routes = solution.routes_number();
 
-            let total = self.stats.total_time().as_secs() ;
-            if routes <= target_routes || total >= TOTAL_TIME {
-                self.stats.print_after_route_removal(solution);
-                solution.eprn();
+            let total = self.stats.total_time();
 
-                for i in 0..solution.data.points {
-                    println!("{} {}", i, solution.heap.removed_times[i]);
+            if routes <= conf.target_routes || total >= conf.max_optimization_time {
+                self.stats.print_after_route_removal(solution);
+                if !matches!(conf.log, Log::Quiet) {
+                    solution.eprn();
+                }
+
+                if let Log::Extra = conf.log {
+                    for i in 0..solution.data.points {
+                        println!("{} {}", i, solution.heap.removed_times[i]);
+                    }
                 }
 
                 break;
@@ -50,9 +68,8 @@ impl<'a> Ges<'a> {
             self.stats.reset();
             self.stats.add_iteration(solution.heap.size);
             while let Some(top) = solution.top() {
-
-                let total = self.stats.total_time().as_secs() ;
-                if total >= TOTAL_TIME {
+                let total = self.stats.total_time();
+                if total >= conf.max_optimization_time {
                     break;
                 }
                 let maybe = solution.try_insert(top, &mut self.evaluator);
@@ -69,10 +86,14 @@ impl<'a> Ges<'a> {
                 }
 
                 self.stats.add_iteration(solution.heap.size);
-                self.stats.print_occasionally(solution);
+                if !matches!(conf.log, Log::Quiet) {
+                    self.stats.print_occasionally(solution);
+                }
             }
 
-            self.stats.print_after_route_removal(solution);
+            if !matches!(conf.log, Log::Quiet) {
+                self.stats.print_after_route_removal(solution);
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
 use std::path::Path;
+use std::time::Duration;
 
 use ges::data::Data;
-use ges::Ges;
 use ges::routes::ROUTES;
+use ges::Ges;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     let path = Path::new(&args[1]);
-    let instance = path.file_stem().unwrap().to_ascii_lowercase().into_string().unwrap();
+    let instance = path
+        .file_stem()
+        .unwrap()
+        .to_ascii_lowercase()
+        .into_string()
+        .unwrap();
     let data = Data::read(path);
 
     let mut solution = ges::sol::Sol::new(&data);
     solution.initialize();
     let mut ges = Ges::new(&data);
 
-    print!("{:?} {} {} ", instance, ROUTES[&instance], ges::TOTAL_TIME);
-    ges.ges(&mut solution, ROUTES[&instance]);
+    let total_time = 600;
+    let log = ges::Log::Quiet;
+
+    let conf = ges::Conf {
+        max_optimization_time: Duration::new(total_time, 0),
+        log,
+        target_routes: ROUTES[&instance],
+    };
+
+    print!(
+        "{:?} {} {} ",
+        instance,
+        conf.target_routes,
+        conf.max_optimization_time.as_secs()
+    );
+
+    ges.ges(&mut solution, conf);
 }
